86e221b6f2ebe7e69b2acece5a5b3c7811c61016
Madison Ehlers <49169871+MadisonEhlers-Vertex@users.noreply.github.com>
2023-04-04 13:47:48 -0500

    [PLAT-2750] Update Stream Key (#99)

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,7 +8,7 @@ export interface StreamCredentials {
 // Wind turbine
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "twGiaVXaTzMX0A7QEQa6KAFGesH7tJM1Fs_7",
+  streamKey: "OHCC5wLnH9SISYJQcaprKZrSbhKU7FSmBAea",
 };
 
 export const Env =
