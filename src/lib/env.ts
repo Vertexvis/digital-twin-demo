d995e48d009b36080d27fa5729d27893bcf26ac2
amvertex <annastacia.mccarty@vertexvis.com>
2026-02-23 10:38:15 -0600

    Update stream key

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,7 +8,7 @@ export interface StreamCredentials {
 // Wind turbine
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "iMO9-NngP3tc3HLdvfn4MdKSmgg33QqpDbgX",
+  streamKey: "kTCq8JWDt3wbQlpE9UhwhA5ZJTBq0_6stUkR",
 };
 
 export const Env =
